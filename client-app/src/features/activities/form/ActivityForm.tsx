743d8a800c860499d99af0e7a95825a409cec585
Ahmet Erdal Yurtseven <erdal.yurtseven.555@gmail.com>
2023-08-09 18:13:58 +0100

    End of section

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,15 +2,15 @@ import React, { ChangeEvent, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { act } from '@testing-library/react';
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
+import { create } from 'domain';
 
-interface Props{
-    activity: Activity | undefined;
-    closeForm: () => void;
-    createOrEdit: (activity: Activity) => void;
-    submitting: boolean;
-}
 
-export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit, submitting} : Props ){
+
+export default observer(function ActivityForm(){
+    const {activityStore} = useStore();
+    const {selectedActivity, closeForm, createActivity, updateActivity, loading} = activityStore;
 
     const intialState = selectedActivity ?? {
         id: '',
@@ -24,7 +24,7 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
 
     const [activity, setActivity] = useState(intialState);
     function handleSubmit(){
-        createOrEdit(activity);
+        activity.id ? updateActivity(activity): createActivity(activity)
     }
 
     function handleInputChange(event : ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
@@ -41,9 +41,9 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
                 <Form.Input type = 'date' placeholder='Date' value = {activity.date} name='date' onChange = {handleInputChange}/>
                 <Form.Input placeholder='City' value = {activity.city} name='city' onChange = {handleInputChange} />
                 <Form.Input placeholder='Venue' value = {activity.venue} name='venue' onChange = {handleInputChange}/>
-                <Button loading ={submitting} floated = 'right' positive type ='submit' content ='submit'/>
+                <Button loading ={loading} floated = 'right' positive type ='submit' content ='submit'/>
                 <Button onClick = {closeForm} floated = 'right' type ='button' content ='Cancel'/>
             </Form>
         </Segment>
     )
-}
+})
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import './app/layout/styles.css';
+import { store, StoreContext } from './app/stores/store'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   //<React.StrictMode>
+  <StoreContext.Provider value={store}>
     <App />
+  </StoreContext.Provider>
+
   //</React.StrictMode>
 );
 
