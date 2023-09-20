73466ec8f6416414b788a08032ac8526492c72ba
Ahmet Erdal Yurtseven <erdal.yurtseven.555@gmail.com>
2023-09-20 17:36:43 +0100

    Section 20

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Domain;
@@ -24,17 +25,18 @@ namespace Application.Activities
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
-
-            public Handler(DataContext context, IMapper mapper)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
                 _context = context;
                 _mapper = mapper;
+                _userAccessor = userAccessor;
             }
 
             public async Task<Result<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities
-                .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider).FirstOrDefaultAsync(x => x.Id == request.Id);
+                .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, new {currentUsername = _userAccessor.GetUserName()}).FirstOrDefaultAsync(x => x.Id == request.Id);
 
                 return Result<ActivityDto>.Success(activity);
             }
--- Application/Profiles/Details.cs
+++ Application/Profiles/Details.cs
@@ -1,4 +1,5 @@
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Domain;
@@ -19,13 +20,15 @@ namespace Application.Profiles
         {
             private readonly IMapper _mapper;
             private readonly DataContext _context;
-            public Handler(DataContext context, IMapper mapper){
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor){
                 _context = context;
                 _mapper = mapper;
+                _userAccessor = userAccessor;
             }
             public async Task<Result<Profile>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var user = await _context.Users.ProjectTo<Profile>(_mapper.ConfigurationProvider).SingleOrDefaultAsync(x => x.UserName == request.Username);    
+                var user = await _context.Users.ProjectTo<Profile>(_mapper.ConfigurationProvider, new{currentUsername = _userAccessor.GetUserName()}).SingleOrDefaultAsync(x => x.UserName == request.Username);    
 
                 if (user == null) return null;
 
